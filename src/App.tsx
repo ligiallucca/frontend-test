cc78e36a8518b26cf764cfc24148ae42b99f0077
ligiallucca <lilillucca@gmail.com>
2024-09-16 07:18:29 -0300

    feat: :construction: InputComponent created

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { theme } from "./config/theme";
 import Login from "./pages/Login/Login";
 import Clients from "./pages/Clients/Clients";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/clients" element={<Clients />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/clients" element={<Clients />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import InputComponent from "../../components/InputComponent/InputComponent";
+
+const StyledForm = styled.form`
+  gap: 20px;
+  width: 100%;
+  padding: 24px;
+  height: 100vh;
+  display: flex;
+  margin: 0 auto;
+  max-width: 98vw;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+
+  @media (min-width: 400px) {
+    max-width: 400px;
+  }
+`;
+
+const StyledTitle = styled.h2`
+  margin: 0;
+  font-weight: 400;
+  font-size: 2.25rem;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.black};
+`;
 
 const Login = () => {
-  return <div>Login Page</div>;
+  const [inputValue, setInputValue] = useState("");
+  const [isInputValid, setIsInputValid] = useState(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
+    setIsInputValid(value.trim() !== "");
+  };
+
+  return (
+    <div className="container">
+      <StyledForm>
+        <StyledTitle>Olá, seja bem-vindo!</StyledTitle>
+        <InputComponent
+          name="userName"
+          placeholder="Digite seu nome"
+          required
+          value={inputValue}
+          onChange={handleInputChange}
+          errorMessage="Nome é obrigatório"
+        />
+      </StyledForm>
+    </div>
+  );
 };
 
 export default Login;
